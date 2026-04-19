60462a19ae0bea029eeb20efe5c52d36e2327f37
Pranav_Sune <pranavsune2@gmail.com>
2026-04-19 19:00:30 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict";    //treat all js code as new version
+
+// alert(3+3)    //we are using node js,not browser
+
+//number => 2
+//bigint
+//string "hi"
+//null => standlone value 
+//undefind =>
+//symbol => unique
+
+//object
+
+console.log(typeof undefined); //undefind
+console.log(typeof null);   //object
