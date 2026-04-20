8bcb1c590766439f2ce3acfe661a75bb76e5286f
pranav.Text <pranavsune2@gmail.com>
2026-04-20 15:29:23 +0530

    concepts clear for the output

---
+++ 01_basic/conversation.js
@@ -0,0 +1,18 @@
+let score= true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber
+)
+
+//"33 => 33
+// "33abc" => nan
+//ture = 1 , false = 0
+
+let isLoggedIn = false
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
