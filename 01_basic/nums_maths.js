a773ce9a0aa1ab4bdbdec5f9c42ea38eb53a78ea
pranav.Text <pranavsune2@gmail.com>
2026-04-21 01:00:58 +0530

    nums complete

---
+++ 01_basic/nums_maths.js
@@ -0,0 +1,19 @@
+const score = 200
+const balance = new Number(300)
+// console.log(balance);
+
+const Balance = new Number(340)
+// console.log(Balance);
+
+// console.log(Balance.toFixed(2));             //340.00
+
+const OtherNumber = 23.343
+
+// console.log(OtherNumber.toPrecision(3));        //23.3
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));      //10,00,000
+
+//-----------Math---------------//
+
+console.log(Math)
